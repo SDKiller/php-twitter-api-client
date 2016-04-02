771b7932e8857ff9353511faeaa59b4db12e9ff7
Marcos Segovia <velozmarkdrea@gmail.com>
2016-04-02 17:12:50 +0200

    Initial commit

---
+++ src/TwitterApiClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MarcosSegovia\Twitter;
+
+final class TwitterApiClient
+{
+
+}
---
+++ tests/TwitterApiClientTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MarcosSegovia\Twitter\Tests;
+
+final class TwitterApiClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function TestItShouldAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
