df354a3f37bf31d61b5817860f04031a00b9c006
Marcos Segovia <velozmarkdrea@gmail.com>
2016-04-03 16:21:11 +0200

    First approach using OAuth2 authentication by Guzzle Client

--- src/TwitterApiClient.php
+++ src/TwitterApiClient.php
@@ -2,7 +2,61 @@
 
 namespace MarcosSegovia\Twitter;
 
-final class TwitterApiClient
+use CommerceGuys\Guzzle\Oauth2\GrantType\ClientCredentials;
+use CommerceGuys\Guzzle\Oauth2\GrantType\RefreshToken;
+use CommerceGuys\Guzzle\Oauth2\Oauth2Subscriber;
+use GuzzleHttp\Client;
+
+/**
+ * Class TwitterApiClient
+ * @package MarcosSegovia\Twitter
+ *
+ * - Search in tweets
+ * - Retrieve any user information
+ */
+class TwitterApiClient
 {
+    const BASE_TWITTER_URL = 'https://api.twitter.com';
+
+    /**
+     * @var Client
+     */
+    protected $auth_client;
+
+    /**
+     * @var Client
+     */
+    protected $http_client;
+
+    /**
+     * @var integer
+     */
+    protected $timeout;
+
+    public function __construct(
+        $a_consumer_key,
+        $a_consumer_secret
+    )
+    {
+        $this->auth_client = new Client(['base_url' => self::BASE_TWITTER_URL]);
+
+        $config_parameters = [
+            'client_id' => $a_consumer_key,
+            'client_secret' => $a_consumer_secret,
+            'token_url' => '/oauth2/token'
+        ];
+
+        $token = new ClientCredentials($this->auth_client, $config_parameters);
+        $refreshToken = new RefreshToken($this->auth_client, $config_parameters);
+
+        $oauth2 = new Oauth2Subscriber($token, $refreshToken);
+
+        $this->http_client = new Client([
+            'defaults' => [
+                'auth' => 'oauth2',
+                'subscribers' => [$oauth2],
+            ],
+        ]);
+    }
 
 }
